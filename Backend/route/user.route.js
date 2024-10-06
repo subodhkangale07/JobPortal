ac1929ab2a2148772ed23a5eb5152157e10e13c9
Subodh kangale <125494916+subodhkangale07@users.noreply.github.com>
2024-10-06 11:20:57 +0530

    dsc

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -6,7 +6,7 @@ import { singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(singleUpload,register);
+router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthentication,updateProfile);
